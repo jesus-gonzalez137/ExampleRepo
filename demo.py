c0e2e99fe5a281d3e5f97b04c3c6e8c060c4bdac
Jesus Manuel Gonzalez <jesusgonzalez@usc-guestwireless-upc-newsc13817.usc.edu>
2019-07-29 10:27:05 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------
+#        Name: Jesus M. Gonzalez
+#        Date: July 29th, 2019
+#
+# Description:  This is a sample file,
+#               including a header and a mai function
+# -------------------------------------------------
+
+
+def main():
+
+    name= "Jesus"
+    print ("Hello, my name is", name)
+
+if __name__ == "__main__":
+    main()
